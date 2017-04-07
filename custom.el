9109c9bc7a3668bd7ebd3dc649ae623de2191c5f
rederry <rederry@outlook.com>
2017-04-07 11:34:57 +0800

    custom

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company sml-mode magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; autopair
+(require 'electric)
+(electric-pair-mode t)
+
+;; linum-mode
+(global-linum-mode t)
+(setq linum-format "%d ")
